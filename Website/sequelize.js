e51f9c09adb50cc687392dbefd339710aad37651
JoshuaJones27 <a21116@alunos.ipca.pt>
2022-03-27 11:11:01 +0100

    Autenticaçção
    Co-authored-by: Rui21137 <Rui21137@users.noreply.github.com>
    Co-authored-by: EduardoR002 <EduardoR002@users.noreply.github.com>

---
+++ Website/sequelize.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('fetchcode', 'FetchCode', 'sidyfgygIJS956_kjhvfddv87', {
+    host: 'fetchcodeserver.mysql.database.azure.com',
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function() {
+    console.log("Conectado com sucesso!")
+}).catch(function(erro) {
+    console.log("Falha a conectar ao servidor: " + erro)
+})
