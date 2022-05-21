dd0f565e221ee3b561d57f936a0d930831a85859
JoshuaJones27 <a21116@alunos.ipca.pt>
2022-05-21 11:54:41 +0100

    Receba

--- Website/api-fetchcode/source/config/routes.js
+++ Website/api-fetchcode/source/config/routes.js
@@ -22,48 +22,48 @@ module.exports = (app) => {
     // CODIGO POSTAL
     app.route('/codigoPostal')
       .all(app.config.passport.authenticate())
-      .get(app.routes.codigosPostais.findAll)
+      .get(app.routes.codigosPostais.getAll)
       .delete(app.routes.codigosPostais.remove);
 
     // COR
     app.route('/cor')
       .all(app.config.passport.authenticate())
-      .get(app.routes.cores.findAll)
+      .get(app.routes.cores.getAll)
       .post(app.routes.cores.create)
       .delete(app.routes.cores.remove);
 
     // ITEM
     app.route('/item')
       .all(app.config.passport.authenticate())
-      .get(app.routes.itens.findAll)
+      .get(app.routes.itens.getAll)
       .post(app.routes.itens.create)
       .delete(app.routes.itens.remove);
 
     // MORADA
     app.route('/morada')
       .all(app.config.passport.authenticate())
-      .get(app.routes.moradas.findAll)
+      .get(app.routes.moradas.getAll)
       .post(app.routes.moradas.create)
       .delete(app.routes.moradas.delete);
  
     // PAGAMENTO
     app.route('/pagamento')
     .all(app.config.passport.authenticate())
-    .get(app.routes.pagamentos.findAll)
+    .get(app.routes.pagamentos.getAll)
     .post(app.routes.pagamentos.create)
     .delete(app.routes.pagamentos.delete);
 
     // TIPOS ITENS
     app.route('/tipoItem')
     .all(app.config.passport.authenticate())
-    .get(app.routes.tipoItens.findAll)
+    .get(app.routes.tipoItens.getAll)
     .post(app.routes.tipoItens.create)
     .delete(app.routes.tipoItens.delete);
 
     // TRANSPORTES
     app.route('/transporte')
     .all(app.config.passport.authenticate())
-    .get(app.routes.transportes.findAll)
+    .get(app.routes.transportes.getAll)
     .post(app.routes.transportes.create)
     .delete(app.routes.transportes.delete);
   };
