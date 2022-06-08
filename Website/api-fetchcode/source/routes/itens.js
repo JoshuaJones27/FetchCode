00d2d6e93d4c17e99b60df7d3f81f76a26ea763c
JoshuaJones27 <a21116@alunos.ipca.pt>
2022-06-08 16:49:46 +0100

    Toma rui

--- Website/api-fetchcode/source/routes/itens.js
+++ Website/api-fetchcode/source/routes/itens.js
@@ -9,12 +9,6 @@ module.exports = (app) => {
             .catch((err) => next(err));
     });
 
-    // router.get('/color/:id', (req, res, next) => {
-    //     app.services.item.findItemByColor( {id: req.params.id} )
-    //     .then((result) => res.status(200).json(result))
-    //     .catch((err) => next(err));
-    // })
-
     router.get('/:id', (req, res, next) => {
         app.services.item.getAllID({ id: req.params.id })
         .then((result) => res.status(200).json(result))
