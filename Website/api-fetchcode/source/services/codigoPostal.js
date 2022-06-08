3b838abdcbc107d8739eb948a4721f528973c864
Rui21137 <a21137@alunos.ipca.pt>
2022-06-08 12:23:18 +0100

    skrt

--- Website/api-fetchcode/source/services/codigoPostal.js
+++ Website/api-fetchcode/source/services/codigoPostal.js
@@ -6,31 +6,31 @@ module.exports = (app) => {
     // };
 
     /**Selecionar todos os códigos postais */
-    const getAll = async () => {
+    const getAll = async() => {
         return app.db('codigopostal').select(['*']);
     };
 
-    /**Filtragem  de apenas dos codigos postais */
-    const getAllCodPost = async (filter) => {
+    /**Filtragem apenas dos codigos postais */
+    const getAllCodPost = async(filter) => {
         return app.db('codigopostal').where(filter).select(['*']);
     };
-    
-    /**Criação do registo de um novo código postal*/ 
-    const create = async (req, res) => {
-        if(!req.codPostal) throw new ValidationError('O Codigo Postal é um campo obrigatorio');
 
-        const newCodigopostal = {...req};
+    /**Criação do registo de um novo código postal*/
+    const create = async(req, res) => {
+        if (!req.codPostal) throw new ValidationError('O Codigo Postal é um campo obrigatorio');
+
+        const newCodigopostal = {...req };
         return app.db('codigopostal').insert(newCodigopostal, ['codPostal']);
     };
 
     /**Atualizar para um novo código postal */
-    const update = async (req, res) => {
-        const newCodigopostal = {...req};
+    const update = async(req, res) => {
+        const newCodigopostal = {...req };
         return app.db('codigopostal').insert(newCodigopostal, ['codPostal']);
     };
 
     /**Remover um código postal */
-    const remove = async (id) => {
+    const remove = async(id) => {
         return app.db('codigopostal').where({ id }).del();
     };
 
