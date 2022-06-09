8ac0118aa2730af6f45d04d0102a984d845fd961
JoshuaJones27 <a21116@alunos.ipca.pt>
2022-06-09 16:26:08 +0100

    idk

--- Website/api-fetchcode/source/routes/moradas.js
+++ Website/api-fetchcode/source/routes/moradas.js
@@ -31,7 +31,7 @@ module.exports = (app) => {
     });
 
     router.delete('/:id', (req, res, next) => {
-        app.services.morada.remove(req.params.id)
+        app.services.morada.remove(req.params.id, req.body)
           .then((result) => res.status(204).json(result[0]))
           .catch((err) => next(err));
     });
